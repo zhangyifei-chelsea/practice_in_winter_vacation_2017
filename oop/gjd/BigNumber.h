6f4812494f72749f49bc9d33f37947b429a73349
tc-imba <liuyh615@sjtu.edu.cn>
2017-02-11 18:25:57 +0800

    gjd more operators
    Signed-off-by: tc-imba <liuyh615@sjtu.edu.cn>

--- oop/gjd/BigNumber.h
+++ oop/gjd/BigNumber.h
@@ -43,21 +43,71 @@ public:
     // Return the length of the BigNumber
     size_t length() const;
 
-    // Return true if the BigNumber is positive
+    // Return whether the BigNumber is positive
     bool isPositive() const;
+
+    // Return the digit on the index (from left to right, excluding the symbol)
+    digit_t operator[](size_t index) const;
+
+    // Return whether two BigNumbers equal
+    bool operator==(BigNumber &that) const;
+
+    // Return whether two BigNumbers unequal
+    bool operator!=(BigNumber &that) const;
+
+    // Return whether one BigNumber is smaller than another
+    bool operator<(BigNumber &that) const;
+
+    // Return whether one BigNumber is not larger than another
+    bool operator<=(BigNumber &that) const;
+
+    // Return whether one BigNumber is larger than another
+    bool operator>(BigNumber &that) const;
+
+    // Return whether one BigNumber is not smaller than another
+    bool operator>=(BigNumber &that) const;
 
     // Add two BigNumbers and return a new one
     BigNumber operator+(const BigNumber &that) const;
 
     // Add two BigNumbers and return the left value
-    BigNumber &operator+=(const BigNumber &that);
+    const BigNumber &operator+=(const BigNumber &that);
+
+    // ++BigNumber
+    BigNumber &operator++();
+
+    // BigNumber++
+    const BigNumber &operator++(int);
 
     // Minus two BigNumbers and return a new one
     BigNumber operator-(const BigNumber &that) const;
 
     // Minus two BigNumbers and return the left value
-    BigNumber &operator-=(const BigNumber &that);
+    const BigNumber &operator-=(const BigNumber &that);
 
+    // --BigNumber
+    BigNumber &operator--();
+
+    // BigNumber--
+    const BigNumber &operator--(int);
+
+    // Multiple two BigNumbers and return a new one
+    BigNumber operator*(const BigNumber &that) const;
+
+    // Multiple two BigNumbers and return the left value
+    const BigNumber &operator*=(const BigNumber &that);
+
+    // Divide two BigNumbers and return a new one
+    BigNumber operator/(const BigNumber &that) const;
+
+    // Divide two BigNumbers and return the left value
+    const BigNumber &operator/=(const BigNumber &that);
+
+    // Mod two BigNumbers and return a new one
+    BigNumber operator%(const BigNumber &that) const;
+
+    // Mod two BigNumbers and return the left value
+    const BigNumber &operator%=(const BigNumber &that);
 
 protected:
     // The vector to save the digits of the BigNumber
