07b36a44ca56cb4e9f12565fee69e13b463c2eb0
tc-imba <liuyh615@sjtu.edu.cn>
2017-02-12 17:55:40 +0800

    gjd more tests
    Signed-off-by: tc-imba <liuyh615@sjtu.edu.cn>

--- oop/gjd/unittest.cpp
+++ oop/gjd/unittest.cpp
@@ -40,7 +40,48 @@ TEST(BigNumber, BasicLonger)
 
 TEST(BigNumber, Addition)
 {
+    BigNumber num_1 = BigNumber("0") + BigNumber("0");
+    BigNumber num_2 = BigNumber("0") + BigNumber("1");
+    BigNumber num_3 = BigNumber("-1") + BigNumber("1");
+    BigNumber num_4 = BigNumber("-1") + BigNumber("0");
+    EXPECT_EQ("0", num_1.toString());
+    EXPECT_EQ("1", num_2.toString());
+    EXPECT_EQ("0", num_3.toString());
+    EXPECT_EQ("-1", num_4.toString());
+}
 
+TEST(BigNumber, AdditionPlusPlus)
+{
+    BigNumber num_1("1234");
+    EXPECT_EQ("1234", num_1++.toString());
+    EXPECT_EQ("1235", num_1.toString());
+    EXPECT_EQ("1235", num_1++.toString());
+    EXPECT_EQ("1237", (++num_1).toString());
+    EXPECT_EQ("1238", (++num_1).toString());
+    EXPECT_EQ("1238", num_1.toString());
+}
+
+TEST(BigNumber, Substraction)
+{
+    BigNumber num_1 = BigNumber("0") - BigNumber("0");
+    BigNumber num_2 = BigNumber("0") - BigNumber("1");
+    BigNumber num_3 = BigNumber("-1") - BigNumber("1");
+    BigNumber num_4 = BigNumber("-1") - BigNumber("0");
+    EXPECT_EQ("0", num_1.toString());
+    EXPECT_EQ("-1", num_2.toString());
+    EXPECT_EQ("-2", num_3.toString());
+    EXPECT_EQ("-1", num_4.toString());
+}
+
+TEST(BigNumber, SubstractionPlusPlus)
+{
+    BigNumber num_1("1234");
+    EXPECT_EQ("1234", num_1--.toString());
+    EXPECT_EQ("1233", num_1.toString());
+    EXPECT_EQ("1233", num_1--.toString());
+    EXPECT_EQ("1231", (--num_1).toString());
+    EXPECT_EQ("1230", (--num_1).toString());
+    EXPECT_EQ("1230", num_1.toString());
 }
 
 int main(int argc, char **argv)
