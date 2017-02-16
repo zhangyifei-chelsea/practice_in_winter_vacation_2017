ac1520d74587861eb8a4369757dc15bd4f72aeb0
zhangyifei-chelsea <zhangyifei-chelsea@sjtu.edu.cn>
2017-02-16 23:42:39 +0800

    .

--- oop/gjd/BigNumber.cpp
+++ oop/gjd/BigNumber.cpp
@@ -7,6 +7,7 @@
 #include <string>
 #include <vector>
 #include "BigNumber.h"
+#include <math.h>
 
 BigNumber::BigNumber()
 {
@@ -30,6 +31,30 @@ BigNumber::BigNumber(const std::string &numStr) {
     }
 }
 
+BigNumber::BigNumber(int64_t num)
+{
+    if (num<0)
+    {
+        m_symbol = NEGATIVE;
+        num=-num;
+    }
+    else
+        m_symbol = POSITIVE;
+    int len = 1;
+    if(num!=0)
+        len=log10(num)+1;
+    for (int i =0; i<len; ++i)
+    {
+        digit_t digit;
+        digit=num%10;
+        num=num/10;
+        m_digits.push_back(digit);
+    }
+    if(len>1) {
+        trim();
+    }
+}
+
 // Copy Constructor
 BigNumber::BigNumber(const BigNumber &that) {
 
@@ -46,7 +71,9 @@ std::string BigNumber::toString() const {
     if (this->m_symbol == NEGATIVE)
         str = "-";
     else if(this->m_symbol==ZERO)
+    {
         str="0";
+    }
     else
         str = "";
     int len = m_digits.size();
@@ -70,6 +97,100 @@ BigNumber BigNumber::operator+(const BigNumber &that) const
      return add(this,&that);
 }
 
+// Return whether two BigNumbers equal
+bool BigNumber::operator==(const BigNumber &that) const
+{
+    int len_a=this->length();
+    int len_b=that.length();
+    SYMBOL symbolA=this->m_symbol;
+    SYMBOL symbolB=that.m_symbol;
+    if (len_a!=len_b||symbolA!=symbolB)
+        return false;
+    else
+    {
+        for(int i=0;i<len_a;++i)
+        {
+            if(this->m_digits[i]!=that.m_digits[i])
+                return false;
+        }
+        return true;
+    }
+}
+
+// Return whether two BigNumbers unequal
+bool BigNumber::operator!=(const BigNumber &that) const
+{
+    return (!(*this==that));
+}
+
+// Return whether one BigNumber is smaller than another
+bool BigNumber::operator<(const BigNumber &that) const
+{
+    BigNumber compare;
+    compare=*this-that;
+    if (compare.m_symbol==NEGATIVE)
+        return true;
+    else
+        return false;
+}
+
+// Return whether one BigNumber is not larger than another
+bool BigNumber::operator<=(const BigNumber &that) const
+{
+    BigNumber compare;
+    compare=*this-that;
+    if (compare.m_symbol==NEGATIVE)
+        return true;
+    else
+    {
+        if(*this==that)
+            return true;
+        else
+            return false;
+    }
+}
+
+// Return whether one BigNumber is larger than another
+bool BigNumber::operator>(const BigNumber &that) const
+{
+    BigNumber compare;
+    compare=*this-that;
+    if (compare.m_symbol==POSITIVE)
+        return true;
+    else
+        return false;
+}
+
+// Return whether one BigNumber is not smaller than another
+bool BigNumber::operator>=(const BigNumber &that) const
+{
+    BigNumber compare;
+    compare=*this-that;
+    if (compare.m_symbol==POSITIVE)
+        return true;
+    else
+    {
+        if(*this==that)
+            return true;
+        else
+            return false;
+    }
+}
+// Reset the BigNumber from a string
+const BigNumber &BigNumber::operator=(const std::string &numStr)
+{
+    *this=numStr;
+    return *this;
+}
+
+// Reset the BigNumber from an integer
+const BigNumber &BigNumber::operator=(const int64_t &num)
+{
+    *this=num;
+    return *this;
+}
+// Same as copy constructor
+
 // ++BigNumber
 BigNumber &BigNumber::operator++()
 {
@@ -88,11 +209,20 @@ const BigNumber BigNumber::operator++(int)
     return c;
 }
 
+const BigNumber &BigNumber::operator+=(const BigNumber &that)
+{
+    *this=*this+that;
+    return *this;
+}
 BigNumber BigNumber::operator-(const BigNumber &that) const
 {
     return add(this,&that,NEGATIVE);
 }
-
+const BigNumber &BigNumber::operator-=(const BigNumber &that)
+{
+    *this=*this-that;
+    return *this;
+}
 // --BigNumber
 BigNumber &BigNumber::operator--()
 {
@@ -113,14 +243,14 @@ const BigNumber BigNumber::operator--(int)
 
 BigNumber BigNumber::operator*(const BigNumber &that) const
 {
-    BigNumber c("0");
-    BigNumber intermediate;
+    BigNumber c;
     digit_t flag=0;
     int len_a,len_b;
     len_a=this->length();
     len_b=that.length();
     for(int i=0;i<len_b;++i)
     {
+        BigNumber intermediate;
         int multiplier;
         multiplier=that.m_digits[i];
         for(int k=0;k<i;++k)
@@ -135,9 +265,32 @@ BigNumber BigNumber::operator*(const BigNumber &that) const
             intermediate.m_digits.push_back(digit);
         }
         intermediate.m_digits.push_back(flag);
+        flag=0;
         c=add(&c,&intermediate);
     }
+    SYMBOL symbolA,symbolB;
+    symbolA=this->m_symbol;
+    symbolB=that.m_symbol;
+    if(symbolA==symbolB)
+        c.m_symbol=POSITIVE;
+    else
+        c.m_symbol=NEGATIVE;
+    c.trim();
     return c;
+}
+const BigNumber &BigNumber::operator*=(const BigNumber &that)
+{
+    *this=*this*that;
+    return *this;
+}
+
+BigNumber BigNumber::operator/(const BigNumber &that) const
+{
+    return divide(*this, that).first;
+}
+BigNumber BigNumber::operator%(const BigNumber &that) const
+{
+    return divide(*this, that).second;
 }
 
 void BigNumber::trim()
@@ -252,8 +405,14 @@ BigNumber BigNumber::add(const BigNumber *numA, const BigNumber *numB, SYMBOL sy
                 c.m_digits.push_back(digit);
             }
             if (c.m_digits.back() < 0) {
-                c.m_digits[0] = 10 - c.m_digits[0];
-                for (int i = 1; i < lenm - 1; ++i)
+                int s;
+                for(s=0;s<lenm-1;++s)
+                {
+                    if(c.m_digits[s]!=0)
+                        break;
+                }
+                c.m_digits[s] = 10 - c.m_digits[s];
+                for (int i = s+1; i < lenm - 1; ++i)
                     c.m_digits[i] = 9 - c.m_digits[i];
                 c.m_digits[lenm - 1] = -c.m_digits[lenm - 1] - 1;
                 c.m_symbol = NEGATIVE;
@@ -291,8 +450,14 @@ BigNumber BigNumber::add(const BigNumber *numA, const BigNumber *numB, SYMBOL sy
             if (c.m_digits.back() < 0)
             {
                 if(c.m_digits.size()>1) {
-                    c.m_digits[0] = 10 - c.m_digits[0];
-                    for (int i = 1; i < len - 1; ++i)
+                    int s;
+                    for(s=0;s<len-1;++s)
+                    {
+                        if(c.m_digits[s]!=0)
+                            break;
+                    }
+                    c.m_digits[s] = 10 - c.m_digits[s];
+                    for (int i = s+1; i < len - 1; ++i)
                         c.m_digits[i] = 9 - c.m_digits[i];
                     c.m_digits[len - 1] = -c.m_digits[len - 1] - 1;
                     c.m_symbol = NEGATIVE;
@@ -313,5 +478,119 @@ BigNumber BigNumber::add(const BigNumber *numA, const BigNumber *numB, SYMBOL sy
     c.trim();
     return c;
 }
+std::pair<BigNumber, BigNumber> BigNumber::divide(const BigNumber &divider, const BigNumber &divisor)
+{
+    int len_divisor=divisor.length();
+    if(len_divisor==1&&divisor.m_digits[0]==0)
+        std::cout<<"Error.";
+    BigNumber factor;
+    BigNumber r;
+    factor.m_symbol=POSITIVE;
+    r=divider;
+    int t=0;
+    BigNumber process;
+    process.m_symbol=POSITIVE;
+    while(r>=divisor)
+    {
+        ++t;
+        if(t==1)
+        {
+            for (int j = len_divisor - 1; j >= 0; --j)
+            {
+                process.m_digits.push_back(r.m_digits[r.length() - 1 - j]);
+            }
+            if (process < divisor)
+            {
+                for (int j = len_divisor - 1; j >= 0; --j)
+                    process.m_digits.pop_back();
+                for (int j = len_divisor; j >= 0; --j)
+                {
+                    process.m_digits.push_back(r.m_digits[r.length() - 1 - j]);
+                }
+            }
+        }
+        else
+        {
+            int l=process.length();
+            //std::cout<<process.toString()<<std::endl;
+            if(l==0)
+            {
+                //std::cout<<"00000";
+                //process.m_digits.push_back(1);
+                process.m_digits.push_back(r.m_digits[r.length()-l-1]);
+                //std::cout<<process.length()<<std::endl;
+                //std::cout<<(int)process.m_digits[0]<<std::endl;
+                //std::cout<<(int)r.m_digits[r.length()-l-1]<<std::endl;
+                //process.trim();
+                process.m_symbol=POSITIVE;
+                std::cout << process.toString()<<std::endl;
+            }
+            else
+            {
+                process.m_digits.push_back(1);
+                for (int i = l - 1; i >= 0; --i)
+                    process.m_digits[i + 1] = process.m_digits[i];
+                process.m_digits[0] = r.m_digits[r.length() - l - 1];
+                //std::cout<<(int)r.m_digits[r.length()-l-1]<<std::endl;
+                //process.m_digits.push_back(r.m_digits[r.length()-l-1]);
+                //BigNumber reverse;
+                //reverse.m_symbol=POSITIVE;
+                //std::cout << process.toString();
+                //break;
+            }
+        }
+        std::cout << t<<"     "<<process.toString()<<std::endl;
+        int len;
+        BigNumber inter,inter2;
+        inter.m_symbol=POSITIVE;
+        inter2.m_symbol=POSITIVE;
+        BigNumber k("0");
+        BigNumber limit("9");
+        for(k;k<=limit;++k)
+        {
+            //std::cout<<"---"<<std::endl;
+            inter=divisor*k;
+            inter2=inter+divisor;
+            BigNumber zero("0");
+            /*if(process==zero)
+            {
+                factor.m_digits.push_back(0);
+                break;
+            }*/
+            if((inter<=process)&&(inter2>process))
+            {
+                digit_t digit;
+                digit=k.m_digits[0];
+                factor.m_digits.push_back(digit);
+                len=inter.length();
+                break;
+            }
+        }
+        int l_p=process.length();
+        process-=inter;
+        BigNumber m;
+        m.m_symbol=POSITIVE;
+        for(int i=0;i<r.length()-l_p;++i)
+        {
+            m.m_digits.push_back(0);
+        }
+        for(int i=0;i<len;++i)
+            m.m_digits.push_back(inter.m_digits[i]);
+        r=r-m;
+        std::cout<<m.toString()<<std::endl;
+        std::cout<<r.toString()<<std::endl;
 
+    }
+    int len_f;
+    len_f=factor.length();
+    BigNumber f;
+    f.m_symbol=POSITIVE;
+    for(int i=0;i<len_f;++i)
+    {
+        f.m_digits.push_back(factor.m_digits[len_f-1-i]);
+    }
+    f.trim();
+    std::cout<<f.toString();
+    return std::pair<BigNumber, BigNumber>(f,r);
+};
 
--- oop/gjd/main.cpp
+++ oop/gjd/main.cpp
@@ -6,9 +6,9 @@ using namespace std;
 int main()
 {
     // Test your class here
-    BigNumber a("10");
-    BigNumber b("11");
-    auto c=a * b;
+    BigNumber a("123456789");
+    BigNumber b("99");
+    auto c=a/b;
     cout<<c.toString();
     return 0;
 }
