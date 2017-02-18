5bff8afd0227a820d7f8a5284972d2d62cc05f67
tc-imba <liuyh615@sjtu.edu.cn>
2017-02-19 00:17:14 +0800

    test script
    Signed-off-by: tc-imba <liuyh615@sjtu.edu.cn>

--- oop/encrypt/main.cpp
+++ oop/encrypt/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <time.h>
 #include "Cipher.h"
 
 using namespace std;
@@ -6,7 +7,14 @@ using namespace std;
 int main()
 {
     // Test your class here
-    Cipher::hashPassword("123001");
-
+    vector<int64_t> vec;
+    srand(0);
+    for (int i = 0; i < 10; i++)
+    {
+        int64_t a = rand(), b = rand();
+        vec.push_back((a << 32) | b);
+    }
+    auto str = Cipher::base64_encode(vec);
+    cout << str << endl;
     return 0;
 }
